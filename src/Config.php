020814c7abe5554aa324d601053b60d785db5c38
Aman Angira <aman@go4businessworld.com>
2019-02-08 16:31:57 +0530

    initialize Config interface and ConfigSkeleton abstract class with gitignore

---
+++ src/Config.php
@@ -0,0 +1,22 @@
+<?php
+	
+	interface Config{
+		
+
+		protected $configFilesPath;
+		protected $extension;
+		
+		/**
+		 * Return the required parameter or entire 
+		 * configuration tree
+		 * @param  string $parameter - full stop seperated string for nested
+		 * and simple string to access entire file tree structure. The
+		 * parameter string should never contain file extension it
+		 * is referencing to.
+		 * @param mixed $default - default value to return if file or 
+		 * require field is not found
+		 * @return mixed - depending on the type of value
+		 * being stored for required parameter
+		 */
+		abstract public function get($parameter, $default);
+	}
---
+++ src/ConfigSkeleton.php
@@ -0,0 +1,65 @@
+<?php
+	
+	namespace Config;
+
+	abstract class ConfigSkeleton implements Config{
+		const PARAMETER_DELIMITER = '.';
+		const NOT_FOUND_FLAG = 'NOTFOUNDFALSE';
+
+		/**
+		 * Returns the path of directory where the class
+		 * would assume the config files to be
+		 * @return string 
+		 */
+		public function getConfigFilesPath(){
+			return $this->configFilesPath;
+		}
+		/**
+		 * Setter for path of directory where the class
+		 * would assume the config files to be
+		 * @return object of extended class
+		 */
+		protected function setConfigFilesPath($path){
+			$this->configFilesPath = $path;
+			return $this;
+		}
+		
+		/**
+		 * Getter for class implementation to get file
+		 * extension of config file
+		 * @return string
+		 */
+		protected function getConfigFileExtension(){
+			return $this->extension;
+		}
+
+		/**
+		 * Setter for class implementation to be invoked
+		 * in constructor to set file extension
+		 * @param string $extension 
+		 */
+		protected function setConfigFileExtension($extension){
+			$this->extension = $extension;
+			return $this;
+		}
+
+		protected function createAbsolutePath($fileName){
+			return $this->getConfigFilesPath() . '/' . $fileName . $this->getConfigFileExtension();
+		}
+
+		protected function getParamsFromString($parameter){
+			if( strpos($parameter, self::PARAMETER_DELIMITER) === false )//if no nesting found return $parameters as filename
+				return [ 
+					$this->createAbsolutePath($parameter) , 
+					[]
+				];
+			$parts = explode(self::PARAMETER_DELIMITER, $parameter);
+			$fileName = trim($parts[0]);
+			array_splice($parts, 0, 1);//Remove file name from array and sanitize indexes
+			return [
+				$this->createAbsolutePath($fileName),
+				$parts
+			];
+		}
+
+	}
